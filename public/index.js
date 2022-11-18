8a896c325b8699c88933b9641268d177e1561827
tarun <raikwartarun72@gmail.com>
2022-11-18 13:50:09 +0530

    sidebar

--- public/index.js
+++ public/index.js
@@ -14,11 +14,36 @@ function typewriter(){
 typewriter();
 
 let contact = document.getElementsByClassName('contact');
+console.log(contact);
 for(let i=0; i<contact.length; i++){
     contact[i].addEventListener('click', ()=>{
-        document.getElementById('contact_info').scrollIntoView();
+        document.getElementById('Contact_page').scrollIntoView();
     })
 }
+
+let client = document.querySelectorAll('#companies div');
+for(let i=0; i<client.length; i++){
+    client[i].addEventListener('mouseover', ()=>{
+        client[i].querySelector('img').style.width = '80%';
+    })
+    client[i].addEventListener('mouseout', ()=>{
+        client[i].querySelector('img').style.width = '60%';
+    })
+}
+
+// **************** side bar *******************
+
+let dropdown = document.getElementById('dropdown');
+
+dropdown.addEventListener('click', ()=>{
+    let sidebar = document.getElementById('sidebar');
+    if(sidebar.style.display == 'block'){
+        sidebar.style.display = 'none';
+    }
+    else{
+        sidebar.style.display = 'block';
+    }
+})
 
 
 let submit = document.getElementById('submit');
